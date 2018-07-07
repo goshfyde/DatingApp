067d4f3172eadb6ec4d02bc6541623c8cb4b716e
Hayden Bush <hayden.p.bush@gmail.com>
2018-07-07 14:09:27 -0400

    Section 1 Lecture 7, Added dotnet watch

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace DatingApp.API.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value4", "value3" };
         }
 
         // GET api/values/5
